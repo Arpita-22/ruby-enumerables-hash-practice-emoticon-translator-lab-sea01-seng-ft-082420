38d2983cd0e5251e93194f2a3b1d71da681bc17c
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-29 00:29:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(file, emoticon)
   data.each do |key,value|
    value.each_value do |inner_value| 
      if inner_value == emoticon
+       binding.pry
        return key
     binding.pry
   #value.each_value do |value| 
